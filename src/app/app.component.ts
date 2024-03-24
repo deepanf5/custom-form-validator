3e74cee71ad4791e81819162c96a2763488d3f11
deepan <deepanappu1999@gmail.com>
2024-03-24 12:17:05 +0530

    creating form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'customValidators';
+  loginForm!:FormGroup
+
+  constructor(private fb:FormBuilder){
+
+    this.loginForm = this.fb.group({
+
+      username:new FormControl('',Validators.required),
+      email:new FormControl('',Validators.required)
+    })
+  }
+
 }
